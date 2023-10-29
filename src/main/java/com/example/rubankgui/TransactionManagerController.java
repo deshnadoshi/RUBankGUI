d7dbe83d55eab78dbb2856419b7d1833847a7da4
deshnadoshi <deshna.doshi@gmail.com>
2023-10-29 18:45:46 -0400

    Adding all GUI component variables to Controller.java class

--- src/main/java/com/example/rubankgui/TransactionManagerController.java
+++ src/main/java/com/example/rubankgui/TransactionManagerController.java
@@ -1,14 +1,168 @@
 package com.example.rubankgui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class TransactionManagerController {
+    // "Open" Tab GUI Components
     @FXML
-    private Label welcomeText;
+    private Tab openTab;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TextField openFname;
+
+    @FXML
+    private TextField openLname;
+
+    @FXML
+    private DatePicker openDOB;
+
+    @FXML
+    private TextField openAmt;
+
+    @FXML
+    private RadioButton openC;
+
+    @FXML
+    private RadioButton openCC;
+
+    @FXML
+    private RadioButton openCCNB;
+
+    @FXML
+    private RadioButton openCCNewark;
+
+    @FXML
+    private RadioButton openCCCamden;
+
+    @FXML
+    private RadioButton openS;
+
+    @FXML
+    private CheckBox openSLoyal;
+
+    @FXML
+    private RadioButton openMM;
+
+    @FXML
+    private Button openSubmit;
+
+    // "Close" Tab GUI Components
+    @FXML
+    private Tab closeTab;
+
+    @FXML
+    private TextField closeFname;
+
+    @FXML
+    private TextField closeLname;
+
+    @FXML
+    private DatePicker closeDOB;
+
+    @FXML
+    private RadioButton closeC;
+
+    @FXML
+    private RadioButton closeCC;
+
+    @FXML
+    private RadioButton closeS;
+
+    @FXML
+    private RadioButton closeMM;
+
+    @FXML
+    private Button closeSubmit;
+
+    // "Deposit" Tab GUI Components
+    @FXML
+    private Tab depTab;
+
+    @FXML
+    private TextField depFname;
+
+    @FXML
+    private TextField depLname;
+
+    @FXML
+    private DatePicker depDOB;
+
+    @FXML
+    private RadioButton depC;
+
+    @FXML
+    private RadioButton depCC;
+
+    @FXML
+    private RadioButton depS;
+
+    @FXML
+    private RadioButton depMM;
+
+    @FXML
+    private TextField depAmt;
+
+    @FXML
+    private Button depSubmit;
+
+    // "Withdraw" Tab GUI Components
+
+    @FXML
+    private Tab withTab;
+
+    @FXML
+    private TextField withFname;
+
+    @FXML
+    private TextField withLname;
+
+    @FXML
+    private DatePicker withDOB;
+
+    @FXML
+    private RadioButton withC;
+
+    @FXML
+    private RadioButton withCC;
+
+    @FXML
+    private RadioButton withS;
+
+    @FXML
+    private RadioButton withMM;
+
+    @FXML
+    private TextField withAmt;
+
+    @FXML
+    private Button withSubmit;
+
+    // "Account Database" Tab GUI Components
+
+    @FXML
+    private Tab acctTab;
+
+    @FXML
+    private Button pPrint;
+
+    @FXML
+    private Button piPrint;
+
+    @FXML
+    private Button ubPrint;
+
+    @FXML
+    private TextArea actDBShow;
+
+
+
+
+
+
+
+
+
+
+
 }
